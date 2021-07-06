b6d7b11a489f00990e9b7bd3c1a3afaebddade12
SashR <sashinramdhani@gmail.com>
2021-07-06 13:57:51 +0200

    Assignment 2 completed

--- basics-assignment-2-problem/app.js
+++ basics-assignment-2-problem/app.js
@@ -1,6 +1,25 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      output: "",
+      output2: "",
+      output3: "",
+    };
   },
-  method: {},
+  methods: {
+    alertFn() {
+      alert("alerted");
+    },
+    regInput(event) {
+      this.output = event.target.value;
+    },
+    regInput2(event) {
+      this.output2 = event.target.value;
+    },
+    regInput3() {
+      this.output3 = this.output2;
+    },
+  },
 });
+
+app.mount("#assignment");
